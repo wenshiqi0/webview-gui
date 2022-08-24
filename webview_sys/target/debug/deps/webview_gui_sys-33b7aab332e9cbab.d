bae9bad752bac95cfcc4658b6b101f75f23934e7
木农 <shiqi.wsq@antgroup.com>
2022-08-24 17:45:17 +0800

    fix: useless code

--- webview_sys/target/debug/deps/webview_gui_sys-33b7aab332e9cbab.d
+++
@@ -1,5 +0,0 @@
-/Users/munong/Documents/github/webview/webview-sys/target/debug/deps/webview_gui_sys-33b7aab332e9cbab.rmeta: src/lib.rs
-
-/Users/munong/Documents/github/webview/webview-sys/target/debug/deps/webview_gui_sys-33b7aab332e9cbab.d: src/lib.rs
-
-src/lib.rs:
--- webview_sys/target/debug/deps/webview_gui_sys-a5ac43c17d606e0f.d
+++
@@ -1,5 +0,0 @@
-/Users/munong/Documents/github/webview/webview-sys/target/debug/deps/webview_gui_sys-a5ac43c17d606e0f.rmeta: src/lib.rs
-
-/Users/munong/Documents/github/webview/webview-sys/target/debug/deps/webview_gui_sys-a5ac43c17d606e0f.d: src/lib.rs
-
-src/lib.rs:
